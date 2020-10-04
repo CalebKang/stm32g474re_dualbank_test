8ace4afccb585c6b08e94ed770522ddffd814b1a
calebkang <kangcaleb@gmail.com>
2020-10-04 15:27:41 +0900

    init

---
+++ Core/Src/fw.h
@@ -0,0 +1,16 @@
+/*
+ * fw.h
+ *
+ *  Created on: Sep 29, 2020
+ *      Author: calebkang
+ */
+
+#ifndef SRC_FW_H_
+#define SRC_FW_H_
+
+const uint32_t fw[] =
+{
+    0x00000000, 0x00000001, 0x00000002, 0x00000003, 0x00000004, 0x00000005,
+};
+
+#endif /* SRC_FW_H_ */
---
+++ Core/Src/main.c
@@ -0,0 +1,292 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "flash_if.h"
+#include "fw.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+extern UART_HandleTypeDef huart2;
+int _write(int32_t file, uint8_t *ptr, int32_t len)
+{
+  if( HAL_UART_Transmit(&huart2, ptr, len, len) == HAL_OK )
+    return len;
+  else
+    return 0;
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+FLASH_OBProgramInitTypeDef    OBInit;
+uint32_t BankActive = 0U;
+uint32_t BankInActive = 0U;
+uint32_t BankSwitch = 0U;
+
+uint32_t checksum = 0;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  if(READ_BIT(SYSCFG->MEMRMP, SYSCFG_MEMRMP_FB_MODE) == SYSCFG_MEMRMP_FB_MODE)
+  {
+    BankActive = FLASH_BANK_2;
+    BankInActive = FLASH_BANK_1;
+  }
+  else
+  {
+    BankActive = FLASH_BANK_1;
+    BankInActive = FLASH_BANK_2;
+  }
+
+  printf("\r\n\r\n\r\nHello World!!!\r\nApplication Bank num : %d\r\n", (int)BankActive);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    HAL_Delay(200);
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+
+    if(BankSwitch == 1)
+    {
+      /************************* Flash writing ************************/
+      printf("Flash erasing...\r\n");
+      if(FLASH_If_Erase(BankInActive) != FLASHIF_OK)
+      {
+        printf("Flash erasing error\r\n");
+        while(1){};
+      }
+#if 1
+      printf("Flash writing...\r\n");
+      __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
+      if(FLASH_If_Write(FLASH_START_BANK2, (uint32_t *)FLASH_START_BANK1, 0x40000) != FLASHIF_OK)
+      {
+        printf("Flash writing error\r\n");
+        while(1){};
+      }
+#else
+      for(int i=FLASH_START_BANK1; i<FLASH_START_BANK1+0x40000; i++)
+      {
+        checksum += *(char *)i;
+      }
+      printf("Flash reading finished : checksum : 0x%x\r\n", (int)checksum);
+#endif
+      printf("Flash writing finished\r\n");
+      /************************* Bank switch ************************/
+      printf("Bank switch!!!\r\n");
+      HAL_FLASH_Unlock();
+
+      /* Clear OPTVERR bit set on virgin samples */
+      __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_OPTVERR);
+
+      /* Allow Access to option bytes sector */
+      HAL_FLASH_OB_Unlock();
+
+      /* Get the Dual boot configuration status */
+      HAL_FLASHEx_OBGetConfig(&OBInit);
+
+      /* Enable/Disable dual boot feature */
+      OBInit.OptionType = OPTIONBYTE_USER;
+      OBInit.USERType   = OB_USER_BFB2;
+
+      if (((OBInit.USERConfig) & (OB_BFB2_ENABLE)) == OB_BFB2_ENABLE)
+      {
+        OBInit.USERConfig = OB_BFB2_DISABLE;
+      }
+      else
+      {
+        OBInit.USERConfig = OB_BFB2_ENABLE;
+      }
+
+      if(HAL_FLASHEx_OBProgram (&OBInit) != HAL_OK)
+      {
+        while (1)
+        {
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+          HAL_Delay(100);
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+          HAL_Delay(2000);
+        }
+      }
+
+      if (HAL_FLASH_OB_Launch() != HAL_OK)
+      {
+        while (1)
+        {
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+          HAL_Delay(100);
+          HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+          HAL_Delay(2000);
+        }
+      }
+
+      HAL_FLASH_OB_Lock();
+      HAL_FLASH_Lock();
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the peripherals clocks
+  */
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  BankSwitch = 1;
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
